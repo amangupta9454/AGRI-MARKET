7885e770806c02bc1ce6fd6242fea9ee6cbc7ccc
CODE NOVA <codenova31@gmail.com>
2026-02-03 14:41:44 +0530

    listing error fixes

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -44,3 +44,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;   // ← this is the key line
